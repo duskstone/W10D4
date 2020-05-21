0dad588cc55e61bbbb3203a86e47d0a0feba350b
duskstone <jedgardromero@gmail.com>
2020-05-21 14:57:06 -0400

    fixed bugged where were invoking ConfigureStore upon variable assignment

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from "./store/store.js"
+
+const store = configureStore;
+
+window.store = store;
+//REMOVE THIS AFTER TESTING IS COMPLETE
 
 document.addEventListener("DOMContentLoaded", () => {
     const content = document.getElementById("content")
