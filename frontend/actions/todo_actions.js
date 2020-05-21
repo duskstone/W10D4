5bb37cd1fc489ae533ec489cb68e26688e04cc2b
duskstone <jedgardromero@gmail.com>
2020-05-21 17:10:50 -0400

    moved exports to top

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -1,15 +1,17 @@
-
-const receiveTodos = todos => ({
-  type: RECEIVE_TODOS,
-  todos: todos
-}); 
-
-const receiveTodo = todo => ({
-  type: RECEIVE_TODO,
-  todo: todo
-}); 
-
-
-export const RECIEVE_TODOS = "RECEIVE_TODOS";
+export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
+
+export const receiveTodos = todos => ({
+  type: RECEIVE_TODOS,
+  todos: todos,
+}); 
+
+export const receiveTodo = todo => ({
+  type: RECEIVE_TODO,
+  todo: todo,
+}); 
+
+
+
+
 
